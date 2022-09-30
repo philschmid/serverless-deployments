17bfa6c1383c11acccb2a585cee713b02c0a6eee
philschmid <schmidphilipp1995@gmail.com>
2022-09-30 23:04:19 +0200

    can list lambda

--- deployer/src/main.rs
+++ deployer/src/main.rs
@@ -1,7 +1,21 @@
+use anyhow::Result;
 use serverless_deploy::deploy;
 
-fn main() {
-    println!("Hello, world!");
-    let x = deploy(1,2);
-    println!("{}",x)
+use clap::Parser;
+/// Simple program to greet a person
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    /// Name of lambda function
+    #[arg(short, long)]
+    function_name: String,
+}
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    tracing_subscriber::fmt::init();
+    let args = Args::parse();
+
+    deploy(args.function_name.as_str()).await?;
+    Ok(())
 }
--- serverless_deploy/src/lib.rs
+++ serverless_deploy/src/lib.rs
@@ -1,14 +1,58 @@
-pub fn deploy(left: usize, right: usize) -> usize {
-    left + right
+use aws_config::{meta::region::RegionProviderChain, SdkConfig};
+use aws_sdk_lambda::{Client, Error};
+use std::env;
+
+const REGION: &str = "us-east-1";
+const AWS_PROFILE: &str = "hf-sm";
+
+async fn get_credentials() -> SdkConfig {
+    match env::var("AWS_ACCESS_KEY_ID") {
+        Ok(_) => (),
+        Err(_) => env::set_var("AWS_PROFILE", AWS_PROFILE),
+    }
+    let region_provider = RegionProviderChain::default_provider().or_else(REGION);
+    aws_config::from_env().region(region_provider).load().await
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+struct LambdaHandler {
+    client: Client,
+}
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+impl LambdaHandler {
+    async fn new() -> Result<Self, Error> {
+        let config = get_credentials().await;
+        let client = Client::new(&config);
+        Ok(Self { client })
+    }
+
+    async fn exists(&self, name: &str) -> Option<aws_sdk_lambda::output::GetFunctionOutput> {
+        let exists = self.client.get_function().function_name(name).send().await;
+
+        match exists {
+            Ok(f) => Some(f),
+            Err(_) => None,
+        }
     }
 }
+
+pub async fn deploy(name: &str) -> Result<(), Error> {
+    let lh = LambdaHandler::new().await?;
+
+    match lh.exists(name).await {
+        Some(_) => println!("{} exists", name),
+        None => println!("{} does not exist", name),
+    }
+
+    Ok(())
+}
+
+// #[cfg(test)]
+// mod tests {
+//     use super::*;
+
+//     #[test]
+//     fn it_works() {
+//         let result = add(2, 2);
+//         assert_eq!(result, 4);
+//     }
+// }
